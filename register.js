d20dd5a5f445265e84d5a4c43c7a50d91795f828
PrinceTew <princeadrielarthurtew@gmail.com>
2024-03-05 16:06:32 +0800

    register-web

---
+++ register.js
@@ -0,0 +1,13 @@
+let name = "Prince Adriel Arthur", 
+lastname = "Tew", 
+age = 19, 
+isActive = true, 
+hobbies = ["Video Games","Creator","Eating"];
+run = function(){
+    console.log("Im Currently Running");
+}
+console.log(name, age, isActive, hobbies);
+console.log(typeof run);
+console.table(hobbies);
+console.log("My name is " + name + " " + lastname)
+console.log(`My name is  ${name} ${lastname} `)
